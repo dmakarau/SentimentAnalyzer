d5791935de74cd59c6ded9bc4a68f27cfa1ab492
dmakarau <denis.makarow@gmail.com>
2025-10-17 06:27:17 +0200

    Initial Commit

---
+++ SentimentAnalyzer/SentimentAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentimentAnalyzerApp.swift
+//  SentimentAnalyzer
+//
+//  Created by Denis Makarau on 17.10.25.
+//
+
+import SwiftUI
+
+@main
+struct SentimentAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
